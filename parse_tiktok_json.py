d250c977334d94685b8454181509693e34c46178
melisilva <55757043+melisilva@users.noreply.github.com>
2023-10-20 16:12:53 +0100

    Finished parsing for the entire file

--- parse_tiktok_json.py
+++ parse_tiktok_json.py
@@ -1,6 +1,6 @@
 import json
 
-with open('user_data.json', 'r', encoding = 'utf-8') as file:
+with open('data/user_data.json', 'r', encoding = 'utf-8') as file:
     data = file.read()
     json_data = json.loads(data)
 
@@ -30,37 +30,167 @@ for video in favorite_videos:
 
 f.close()
 
-"""
+
 # Extract Followers List
 followers_list = json_data["Activity"]["Follower List"]["FansList"]
+
+filename = "data/followers.csv"
+f = open(filename, "w")
+f.write("Date,UserName\n")
+for follower in followers_list:
+    date = follower["Date"]
+    username = follower["UserName"]
+
+    f.write(date + "," + username + "\n")
+
+f.close()
 
 # Extract Following list
 following_list = json_data["Activity"]["Following List"]["Following"]
 
-# Extract Hashtags used list
-hashtag_list = json_data["Activity"]["Hashtag"]["HashtagList"]
+filename = "data/following.csv"
+f = open(filename, "w")
+f.write("Date,UserName\n")
+for follower in followers_list:
+    date = follower["Date"]
+    username = follower["UserName"]
+
+    f.write(date + "," + username + "\n")
+
+f.close()
 
 # Extract Likes list
 likes_list = json_data["Activity"]["Like List"]["ItemFavoriteList"]
 
-# Extract Search List
-search_list = json_data["Activity"]["Search History"]["SearchList"]
+# Put through the same scrapper as the favorite videos
+filename = "data/likes.csv"
+f = open(filename, "w")
+f.write("Date,Link\n")
+for like in likes_list:
+    date = like["Date"]
+    link = like["Link"]
+
+    f.write(date + "," + link + "\n")
+
+f.close()
+
+# Extract Hashtags used list
+hashtag_list = json_data["Activity"]["Hashtag"]["HashtagList"]
+
+# Don't really know what we can/should scrape from this or we just don't?
+filename = "data/hashtags.csv"
+f = open(filename, "w")
+f.write("HashtagName,HashtagLink\n")
+for hashtag in hashtag_list:
+    hashtag_name = hashtag["HashtagName"]
+    hashtag_link = hashtag["HashtagLink"]
+
+    f.write(hashtag_name + "," + hashtag_link + "\n")
+
+f.close()
 
 # Extract Login History List
 login_history_list = json_data["Activity"]["Login History"]["LoginHistoryList"]
 
+filename = "data/login_history.csv"
+f = open(filename, "w")
+f.write("Date,IP,DeviceModel,DeviceSystem,NetworkType,Carrier\n")
+for login in login_history_list:
+    date = login["Date"]
+    ip = login["IP"]
+    device_model = login["DeviceModel"]
+    device_system = login["DeviceSystem"]
+    network_type = login["NetworkType"]
+    carrier = login["Carrier"]
+
+    f.write(date + "," + ip + "," + device_model + "," + device_system + "," + network_type + "," + carrier + "\n")
+
+f.close()
+
+
+# Extract Search List
+search_list = json_data["Activity"]["Search History"]["SearchList"]
+
+# Don't really know what we can/should scrape from this or we just don't?
+filename = "data/search_history.csv"
+f = open(filename, "w")
+f.write("Date,SearchTerm\n")
+for search in search_list:
+    date = search["Date"]
+    search_term = search["SearchTerm"]
+
+    f.write(date + "," + search_term + "\n")
+
+f.close()
+
 # Extract Share History List
 share_history_list = json_data["Activity"]["Share History"]["ShareHistoryList"]
 
+# We can scrape stuff from the link but we might not be that worth it?
+filename = "data/share_history.csv"
+f = open(filename, "w")
+f.write("Date,SharedContent,Link,Method\n")
+for share in share_history_list:
+    date = share["Date"]
+    shared_content = share["SharedContent"]
+    link = share["Link"]
+    method = share["Method"]
+
+    f.write(date + "," + shared_content + "," + link + "," + method + "\n")
+
+f.close()
+
 # Extract Video Browsing History List
-video_browsing_history_list = json_data["Video Browsing History"]["VideoList"]
+video_browsing_history_list = json_data["Activity"]["Video Browsing History"]["VideoList"]
+
+filename = "data/video_browsing_history.csv"
+f = open(filename, "w")
+f.write("Date,Link\n")
+for video in video_browsing_history_list:
+    date = video["Date"]
+    link = video["Link"]
+
+    f.write(date + "," + link + "\n")
+
+f.close()
 
 # Extract Video Upload History List
 video_upload_history_list = json_data["Video"]["Videos"]["VideoList"]
 
+#We can scrape this but idk how worth it, it is
+filename = "data/video_upload_history.csv"
+f = open(filename, "w")
+f.write("Date,Link,Likes\n")
+for video in video_upload_history_list:
+    date = video["Date"]
+    link = video["Link"]
+    likes = video["Likes"]
+
+    f.write(date + "," + link + "," + likes + "\n")
+
+f.close()
+
 # Extract Comments List
 comments_list = json_data["Comment"]["Comments"]["CommentsList"]
 
+filename = "data/comments.csv"
+f = open(filename, "w",encoding="utf-8")
+f.write("Date,Comment\n")
+for comment in comments_list:
+    date = comment["Date"]
+    comment = comment["Comment"]
+
+    f.write(date + "," + comment + "\n")
+
+f.close()
+
 # Extract Ad Interests List
-ad_interests_list = json_data["Ads and data"]["Ad Interests"]["AdInterestCategories"]
-"""
+ad_interests_list = json_data["Ads and data"]["Ad Interests"]["AdInterestCategories"] # this is just strings
+
+filename = "data/ad_interests.csv"
+f = open(filename, "w")
+f.write("AdInterestCategories\n")
+for ad_interest in ad_interests_list:
+    f.write(ad_interest + "\n")
+
+f.close()
