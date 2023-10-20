b82efe43cf0d1183f8e52f1cb62c767c7aac7a13
melisilva <55757043+melisilva@users.noreply.github.com>
2023-10-20 16:18:52 +0100

    Made tiktok_scrapper more generic

--- tiktok_scrapper.py
+++ tiktok_scrapper.py
@@ -9,65 +9,70 @@ import os
 
 header = ["Date", "Link", "Username", "Description", "Hashtags", "Sound Name", "Sound Link"]
 
-csv_file_path = "favorite_videos.csv"
-csv_file_path_to_save = "tiktok_data.csv"
-
-table = pd.read_csv(csv_file_path)
-row = 0
-
-if os.path.exists(csv_file_path_to_save):
-    # check what's the last line
-    df = pd.read_csv(csv_file_path_to_save)
-    last_line = df.tail(1)
-
-    # find the row in the csv_file_path
-    matching_rows = table.loc[table['Link'] == last_line['Link'].values[0]]
-    row = matching_rows.index.values[0] + 2
-
-sliced_table = table.iloc[row:,:]
-
-for index, row in sliced_table.iterrows():
-    date = row['Date']
-    link = row['Link']
-    page = requests.get(link)
-
-    soup = BeautifulSoup(page.content, "lxml")
-    error_info = soup.find("div", {"class":"tiktok-u2vwc1-DivErrorWrapper elnrzms1"})
-    if(error_info != None):
-        print("Error: Video not found")
-        continue
-    tiktok_info = soup.find("div", {"class":"tiktok-15ip68j-DivDescriptionContentWrapper-StyledDetailContentWrapper eqrezik14"})
-    # print(tiktok_info)
-    tiktok_username = tiktok_info.find("span", {"class": "tiktok-1c7urt-SpanUniqueId evv7pft1"}).text
-    tiktok_description = tiktok_info.find("span", {"class":"tiktok-j2a19r-SpanText efbd9f0"})
-    if(tiktok_description != None):
-        tiktok_description = tiktok_description.text
-    else:
-        tiktok_description = ""
+def get_tiktok_video_info(input_file_path, output_file_path):
 
 
-    hashtag_links = tiktok_info.find_all('a', href=True)
-
-    hashtags = [link.text.strip() for link in hashtag_links if link.text.strip().startswith('#')]
-    print(hashtags)
-
-    sound_name = tiktok_info.find("h4")
-    if(sound_name != None):
-        sound_name = sound_name.text
-        sound_link = "www.tiktok.com"+tiktok_info.find("h4").find("a")['href']
-    else:
-        sound_name = ""
-        sound_link = ""
+    table = pd.read_csv(input_file_path)
+    row = 0
     
+    if os.path.exists(output_file_path):
+        # check what's the last line
+        df = pd.read_csv(output_file_path)
+        last_line = df.tail(1)
+    
+        # find the row in the input_file_path
+        matching_rows = table.loc[table['Link'] == last_line['Link'].values[0]]
+        row = matching_rows.index.values[0] + 2
+    
+    sliced_table = table.iloc[row:,:]
+    
+    for index, row in sliced_table.iterrows():
+        date = row['Date']
+        link = row['Link']
+        page = requests.get(link)
+    
+        soup = BeautifulSoup(page.content, "lxml")
+        error_info = soup.find("div", {"class":"tiktok-u2vwc1-DivErrorWrapper elnrzms1"})
+        if(error_info != None):
+            print("Error: Video not found")
+            continue
+        tiktok_info = soup.find("div", {"class":"tiktok-15ip68j-DivDescriptionContentWrapper-StyledDetailContentWrapper eqrezik14"})
+        # print(tiktok_info)
+        tiktok_username = tiktok_info.find("span", {"class": "tiktok-1c7urt-SpanUniqueId evv7pft1"}).text
+        tiktok_description = tiktok_info.find("span", {"class":"tiktok-j2a19r-SpanText efbd9f0"})
+        if(tiktok_description != None):
+            tiktok_description = tiktok_description.text
+        else:
+            tiktok_description = ""
+    
+    
+        hashtag_links = tiktok_info.find_all('a', href=True)
+    
+        hashtags = [link.text.strip() for link in hashtag_links if link.text.strip().startswith('#')]
+        print(hashtags)
+    
+        sound_name = tiktok_info.find("h4")
+        if(sound_name != None):
+            sound_name = sound_name.text
+            sound_link = "www.tiktok.com"+tiktok_info.find("h4").find("a")['href']
+        else:
+            sound_name = ""
+            sound_link = ""
+        
+    
+        tiktok_data = [date, link, tiktok_username, tiktok_description, ', '.join(hashtags), sound_name, sound_link]
+    
+        # Check if the CSV file exists or not and write the data to it
+    
+        with open(output_file_path, 'a', newline='', encoding='utf-8') as csv_file:
+            writer = csv.writer(csv_file)
+            if(csv_file.tell() == 0):
+                writer.writerow(header)
+            writer.writerow(tiktok_data)
+            print(f"Data appended to {output_file_path}")
 
-    tiktok_data = [date, link, tiktok_username, tiktok_description, ', '.join(hashtags), sound_name, sound_link]
-    csv_file_path_to_save = "tiktok_data.csv"
-
-    # Check if the CSV file exists or not and write the data to it
-
-    with open(csv_file_path_to_save, 'a', newline='', encoding='utf-8') as csv_file:
-        writer = csv.writer(csv_file)
-        if(csv_file.tell() == 0):
-            writer.writerow(header)
-        writer.writerow(tiktok_data)
-        print(f"Data appended to {csv_file_path_to_save}")
+get_tiktok_video_info("data/favorite_videos.csv", "data/favorite_videos_full_tiktok_data.csv")
+get_tiktok_video_info("data/likes.csv","data/likes_full_tiktok_data.csv")
+# get_tiktok_video_info("data/share_history.csv", "data/share_history_full_tiktok_data.csv") - we need to change the header to have the rest of the stuff
+get_tiktok_video_info("data/video_browsing_history.csv", "data/video_browsing_history_full_tiktok_data.csv")
+#get_tiktok_video_info("data/video_upload_history.csv", "data/video_upload_history_full_tiktok_data.csv") # we need to change the header to have the rest of the stuff
