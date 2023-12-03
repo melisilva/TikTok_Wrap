493e4d0d52ca7acdc6543a7d5016a68bcba6e007
melisilva <55757043+melisilva@users.noreply.github.com>
2023-12-03 21:48:26 +0000

    Top Ad Creators (WIP)

--- frontend/my-vue-app/src/views/TopAdCreators.vue
+++ frontend/my-vue-app/src/views/TopAdCreators.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>Top Ad Creators</h1>
+    <img class="tik-tok-logo" src="../assets/images/tiktok-logo.png" />
+    <div class="title">
+        40% of TikToks you watched were ads
+    </div>
+    <div class="subtitle">
+        Top Ad Creators
+    </div>
+
+    <div class="creator-item">
+        <div class="number"> #1 </div>
+        <img class="pfp" src="../assets/images/ellipse-10.jpg" />
+        <div class="name"> batmanbff </div>
+    </div>
+
+    <div class="creator-item">
+        <div class="number"> #2 </div>
+        <img class="pfp" src="../assets/images/ellipse-10.jpg" />
+        <div class="name"> batmanbff </div>
+    </div>
+
+    <div class="creator-item">
+        <div class="number"> #3 </div>
+        <img class="pfp" src="../assets/images/ellipse-10.jpg" />
+        <div class="name"> batmanbff </div>
+    </div>
+
+    <div class="creator-item">
+        <div class="number"> #4 </div>
+        <img class="pfp" src="../assets/images/ellipse-10.jpg" />
+        <div class="name"> batmanbff </div>
+    </div>
+
+    <div class="creator-item">
+        <div class="number"> #5 </div>
+        <img class="pfp" src="../assets/images/ellipse-10.jpg" />
+        <div class="name"> batmanbff </div>
+    </div>
 </template>
