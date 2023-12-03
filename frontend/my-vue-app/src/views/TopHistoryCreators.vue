be7c4c60bec0b59a44d5a9e5ae12e326f7f4384f
melisilva <55757043+melisilva@users.noreply.github.com>
2023-12-03 19:22:17 +0000

    Progress in TopHistoryCreators

--- frontend/my-vue-app/src/views/TopHistoryCreators.vue
+++ frontend/my-vue-app/src/views/TopHistoryCreators.vue
@@ -5,9 +5,18 @@
      <div class="side-by-side-chart">
      <div class="rectangle" id="rect-15"></div>
      <div class="rectangle" id="rect-12"></div>
-     <div class="rectangle" id="rect-11"></div>
+     <div class="rectangle" id="rect-11">
+     </div>
+     <div class="_1st-place" >
+<img class="ellipse-1" src="../assets/images/ellipse-10.jpg" />
+<div class="rectangle-11" >
+</div>
+<div class="_1" >
+#1 </div>
+</div> 
     <div class="rectangle" id="rect-13"></div>
     <div class="rectangle" id="rect-14"></div>
      </div>
+     
  </body>
 </template>
