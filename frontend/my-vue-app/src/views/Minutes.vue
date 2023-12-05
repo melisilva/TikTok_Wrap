fb1573983ff4988a6929aff4e4142f6b6f8bf8a3
melisilva <55757043+melisilva@users.noreply.github.com>
2023-12-05 18:18:27 +0000

    Fix minutes page

--- frontend/my-vue-app/src/views/Minutes.vue
+++ frontend/my-vue-app/src/views/Minutes.vue
@@ -1,18 +1,27 @@
 <template>
-    <div>
+  <div class="page"
+    style="background-image: linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)), url('/images/tiktok_pattern.png');">
       <img class="tik-tok-logo" src="../assets/images/tiktok-logo.png" />
-      <div class="general-text">
+      <div style="display: flex; flex-direction: column; justify-content: center; align-items: middle;">
+      <div class="row">
+        <div class="general-text" style="padding-top: 15%;">
         You watched...
       </div>
-
-      <div class="general-text">
-        ...{{ totalMinutes }} minutes
       </div>
 
+      <div class="row">
+        <div class="general-text" style="margin-top:5%;">
+        ...{{ totalMinutes }} minutes
+      </div>
+      </div>
+
+      <div class="row" style="display: flex; flex-direction: row; justify-content: center;">
       <div class="quote-text">
         {{ quote }}
       </div>
+      </div>
     </div>
+  </div>
   </template>
   
   <script>
