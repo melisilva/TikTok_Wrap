d4ffa2e160a9c0971a17266aae4073437adc3464
melisilva <55757043+melisilva@users.noreply.github.com>
2023-12-05 18:31:13 +0000

    Fix AdsMessage page

--- frontend/my-vue-app/src/views/AdsMessage.vue
+++ frontend/my-vue-app/src/views/AdsMessage.vue
@@ -2,9 +2,13 @@
     <div class="page" style="background:linear-gradient(180deg, rgba(255, 68, 105, 0.83) 0%,rgba(33, 186, 235, 0.86) 53.5416305065155%);"> 
         <img class="tik-tok-logo" src="../assets/images/tiktok-logo.png" />  
         
-        <div class="ads-message">
+        <div>
+            <div class="row" style="padding-left: 35%;">
+                <div class="general-text" style="padding-top: 30%; width: 50%;">
             Bet you were bothered by some ads during your time on TikTok...but who were those companies?
-        </div>
+                </div>
+            </div>
 
+    </div>
     </div>
 </template>
