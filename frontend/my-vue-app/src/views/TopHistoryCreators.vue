19a084b6a7fbc6cd19de9443f876f7eeaea3934a
melisilva <55757043+melisilva@users.noreply.github.com>
2023-12-05 19:14:53 +0000

    Connect TopHistoryCreators to backend

--- frontend/my-vue-app/src/views/TopHistoryCreators.vue
+++ frontend/my-vue-app/src/views/TopHistoryCreators.vue
@@ -5,49 +5,58 @@
           <div class="side-by-side-chart" style="flex-direction: row;">
                <div class="col">
                     <div class="place" style="left:-5px;">
-                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <img class="ellipse" v-bind:src="photos[4] === '' ? 'src/assets/images/user_default.png' : photos[4]" />
                          <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
                               <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#5</p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-15"></div>
+                    <div class="rectangle" id="rect-15">
+                         <p class="text-inside-rectangle"> {{ creators[4]}} </p>
+                    </div>
                </div>
                <div class="col">
                     <div class="place" style="left:-5px;">
-                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <img class="ellipse" v-bind:src="photos[1] === '' ? 'src/assets/images/user_default.png' : photos[1]" />
                          <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
                               <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#2</p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-12"></div>
+                    <div class="rectangle" id="rect-12">
+                         <p class="text-inside-rectangle"> {{ creators[1]}} </p>
+                    </div>
                </div>
                <div class="col">
                     <div class="place">
-                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <img class="ellipse" v-bind:src="photos[0] === '' ? 'src/assets/images/user_default.png' : photos[0]" />
                          <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
                               <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#1</p>
                          </div>
                     </div>
                     <div class="rectangle" id="rect-11">
+                         <p class="text-inside-rectangle"> {{ creators[0]}} </p>
                     </div>
                </div>
                <div class="col">
                     <div class="place" style="left:5px;">
-                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <img class="ellipse" v-bind:src="photos[2] === '' ? 'src/assets/images/user_default.png' : photos[2]" />
                          <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
                               <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#3</p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-13"></div>
+                    <div class="rectangle" id="rect-13">
+                         <p class="text-inside-rectangle"> {{ creators[2]}} </p>
+                    </div>
                </div>
                <div class="col">
                     <div class="place" style="left:5px;">
-                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <img class="ellipse" v-bind:src="photos[3] === '' ? 'src/assets/images/user_default.png' : photos[3]" />
                          <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
                               <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#4</p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-14"></div>
+                    <div class="rectangle" id="rect-14">
+                         <p class="text-inside-rectangle"> {{ creators[3]}} </p>
+                    </div>
                </div>
 
 
@@ -55,3 +64,31 @@
 
      </div>
 </template>
+
+<script>
+import axios from 'axios'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TopHistoryCreators',
+  data() {
+    return {
+     creators: [],
+     photos: []
+    }
+  },
+  methods: {
+  },
+  async beforeMount() {
+    await axios.get("http://localhost:8000/top-creator-history")
+      .then((response) => {
+        console.log(response.data)
+        this.creators = Object.values(response.data['Username']);
+        this.photos = Object.values(response.data['Photo']);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+})
+</script>
