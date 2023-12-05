202e9043fa7edc5f17a5bc6079b7b4f60a8bdc3c
melisilva <55757043+melisilva@users.noreply.github.com>
2023-12-05 18:59:16 +0000

    Touch ups in TopHistoryCreators

--- frontend/my-vue-app/src/views/TopHistoryCreators.vue
+++ frontend/my-vue-app/src/views/TopHistoryCreators.vue
@@ -4,15 +4,27 @@
           <img class="tik-tok-logo" src="../assets/images/tiktok-logo.png" />
           <div class="side-by-side-chart" style="flex-direction: row;">
                <div class="col">
+                    <div class="place" style="left:-5px;">
+                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
+                              <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#5</p>
+                         </div>
+                    </div>
                     <div class="rectangle" id="rect-15"></div>
                </div>
                <div class="col">
+                    <div class="place" style="left:-5px;">
+                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
+                              <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#2</p>
+                         </div>
+                    </div>
                     <div class="rectangle" id="rect-12"></div>
                </div>
-               <div class="col" style="display: flex; flex-direction: column; align-items: flex-end;">
-                    <div class="_1st-place">
-                         <img class="ellipse-1" src="../assets/images/ellipse-10.jpg" />
-                         <div class="rectangle-11" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
+               <div class="col">
+                    <div class="place">
+                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
                               <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#1</p>
                          </div>
                     </div>
@@ -20,9 +32,21 @@
                     </div>
                </div>
                <div class="col">
+                    <div class="place" style="left:5px;">
+                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
+                              <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#3</p>
+                         </div>
+                    </div>
                     <div class="rectangle" id="rect-13"></div>
                </div>
                <div class="col">
+                    <div class="place" style="left:5px;">
+                         <img class="ellipse" src="../assets/images/ellipse-10.jpg" />
+                         <div class="rectangle-place" style=" width: auto; left: 25px; top: -90px; z-index: 1; padding: 2px 5px 2px 5px;">
+                              <p style="padding: 0px; margin: 0px; font-family: 'futura-maxi-cg-bold'; color: white; ">#4</p>
+                         </div>
+                    </div>
                     <div class="rectangle" id="rect-14"></div>
                </div>
 
