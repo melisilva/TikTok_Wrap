10a955c47b2a12993d40b31d3cca2374cd82cebe
melisilva <55757043+melisilva@users.noreply.github.com>
2023-12-14 17:18:57 +0000

    Top History Creator working

--- frontend/my-vue-app/src/views/TopHistoryCreators.vue
+++ frontend/my-vue-app/src/views/TopHistoryCreators.vue
@@ -1,65 +1,70 @@
 <template>
+     <img class="tik-tok-logo" src="../assets/images/tiktok-logo.png" />
      <div class="page" style="background: #b4b5db;">
-          <div class="top-history-creators"> Your Top 5 Watched Creators</div>
-          <img class="tik-tok-logo" src="../assets/images/tiktok-logo.png" />
-          <div class="side-by-side-chart" style="flex-direction: row; width: 100%; ">
-               <div class="col" >
-                    <div class="place ranking">
-                         <img class="ellipse" v-bind:src="photos[4] === '' ? 'src/assets/images/user_default.png' : photos[4]" />
-                         <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
-                              <p class="number-placement">#5</p>
+          <div class="row">
+               <div class="top-history-creators" style="margin-bottom: 0px;"> Your Top 5 Watched Creators</div>
+          </div>
+          <br style="margin-bottom: 10px; "/>
+          <div class="row">
+               <div class="side-by-side-chart" style="flex-direction: row; width: 100%; ">
+                    <div class="col ranking" >
+                         <div class="place ranking">
+                              <img class="ellipse ranking" v-bind:src="photos[4] === '' ? 'src/assets/images/user_default.png' : photos[4]" />
+                              <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
+                                   <p class="number-placement">#5</p>
+                              </div>
+                         </div>
+                         <div class="rectangle" id="rect-15">
+                              <p class="text-inside-rectangle"> {{ creators[4]}} </p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-15">
-                         <p class="text-inside-rectangle"> {{ creators[4]}} </p>
-                    </div>
-               </div>
-               <div class="col ranking">
-                    <div class="place ranking">
-                         <img class="ellipse" v-bind:src="photos[1] === '' ? 'src/assets/images/user_default.png' : photos[1]" />
-                         <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
-                              <p class="number-placement">#2</p>
+                    <div class="col ranking">
+                         <div class="place ranking">
+                              <img class="ellipse ranking" v-bind:src="photos[1] === '' ? 'src/assets/images/user_default.png' : photos[1]" />
+                              <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
+                                   <p class="number-placement">#2</p>
+                              </div>
+                         </div>
+                         <div class="rectangle" id="rect-12">
+                              <p class="text-inside-rectangle"> {{ creators[1]}} </p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-12">
-                         <p class="text-inside-rectangle"> {{ creators[1]}} </p>
-                    </div>
-               </div>
-               <div class="col ranking">
-                    <div class="place ranking">
-                         <img class="ellipse" v-bind:src="photos[0] === '' ? 'src/assets/images/user_default.png' : photos[0]" />
-                         <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
-                              <p class="number-placement">#1</p>
+                    <div class="col ranking">
+                         <div class="place ranking">
+                              <img class="ellipse ranking" v-bind:src="photos[0] === '' ? 'src/assets/images/user_default.png' : photos[0]" />
+                              <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
+                                   <p class="number-placement">#1</p>
+                              </div>
+                         </div>
+                         <div class="rectangle" id="rect-11">
+                              <p class="text-inside-rectangle"> {{ creators[0]}} </p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-11">
-                         <p class="text-inside-rectangle"> {{ creators[0]}} </p>
-                    </div>
-               </div>
-               <div class="col ranking">
-                    <div class="place ranking">
-                         <img class="ellipse" v-bind:src="photos[2] === '' ? 'src/assets/images/user_default.png' : photos[2]" />
-                         <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
-                              <p class="number-placement">#3</p>
+                    <div class="col ranking">
+                         <div class="place ranking">
+                              <img class="ellipse ranking" v-bind:src="photos[2] === '' ? 'src/assets/images/user_default.png' : photos[2]" />
+                              <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
+                                   <p class="number-placement">#3</p>
+                              </div>
+                         </div>
+                         <div class="rectangle" id="rect-13">
+                              <p class="text-inside-rectangle"> {{ creators[2]}} </p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-13">
-                         <p class="text-inside-rectangle"> {{ creators[2]}} </p>
-                    </div>
-               </div>
-               <div class="col ranking">
-                    <div class="place ranking">
-                         <img class="ellipse" v-bind:src="photos[3] === '' ? 'src/assets/images/user_default.png' : photos[3]" />
-                         <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
-                              <p class="number-placement">#4</p>
+                    <div class="col ranking">
+                         <div class="place ranking">
+                              <img class="ellipse ranking" v-bind:src="photos[3] === '' ? 'src/assets/images/user_default.png' : photos[3]" />
+                              <div class="rectangle-place" style=" width: auto; top: -22.5px; z-index: 1; padding: 2px 5px 2px 5px;">
+                                   <p class="number-placement">#4</p>
+                              </div>
+                         </div>
+                         <div class="rectangle" id="rect-14">
+                              <p class="text-inside-rectangle"> {{ creators[3]}} </p>
                          </div>
                     </div>
-                    <div class="rectangle" id="rect-14">
-                         <p class="text-inside-rectangle"> {{ creators[3]}} </p>
-                    </div>
+     
+     
                </div>
-
-
           </div>
      </div>
 </template>
