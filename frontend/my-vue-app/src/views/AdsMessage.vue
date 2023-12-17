b0456c8c64e258f9610c11e1efbe7444c0210d24
melisilva <55757043+melisilva@users.noreply.github.com>
2023-12-17 17:26:12 +0000

    Fix AdsMessage

--- frontend/my-vue-app/src/views/AdsMessage.vue
+++ frontend/my-vue-app/src/views/AdsMessage.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="page" style="background:linear-gradient(180deg, rgba(255, 68, 105, 0.83) 0%,rgba(33, 186, 235, 0.86) 53.5416305065155%); text-align: center;"> 
+    <div class="page" style="background:linear-gradient(180deg, rgba(255, 68, 105, 0.83) 0%,rgba(33, 186, 235, 0.86) 53.5416305065155%);text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;"> 
         <img class="tik-tok-logo" src="../assets/images/tiktok-logo.png" />  
         
-        <div>
-            <div class="row" style="padding-left: 30%;">
-                <div class="general-text" style="padding-top: 30%; width: 50%;">
-            Bet you were bothered by some ads during your time on TikTok ... but who were those companies?
-                </div>
+        <div class="row" style="width: 80%;">
+            <div class="general-text" style="text-align: center;">
+                Bet you were bothered by some ads during your time on TikTok....
             </div>
-
-    </div>
+            <div class="general-text" style="padding-top: 20%;">
+                ...but who were those companies?
+            </div>
+        </div>
     </div>
 </template>
